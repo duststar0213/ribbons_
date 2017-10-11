f8cb0d607885749690b9b385be3bc60889bb6da7
hannahliao <duststar950213@gmail.com>
2017-10-11 14:38:04 -0700

    first commit to put ribbons.pde on github

---
+++ application.macosx64/source/ribbons_.java
@@ -0,0 +1,161 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import ddf.minim.*; 
+import ddf.minim.analysis.*; 
+import ddf.minim.effects.*; 
+import ddf.minim.signals.*; 
+import ddf.minim.spi.*; 
+import ddf.minim.ugens.*; 
+import processing.video.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class ribbons_ extends PApplet {
+
+
+
+
+
+
+
+
+
+float diameter=0;
+float angle =0;
+float c1 =0 ;
+PImage img;
+Capture cam;
+float theta = 0;
+  float r = 0;
+boolean direction = true;
+public void setup(){
+  
+  background(255);
+  frameRate(10);
+  
+
+  //colorMode(HSB);
+  surface.setResizable(true);
+  
+}
+public void draw()
+{
+background(255);
+translate(width/2,height/2);
+   pushMatrix();
+for(float i =0; i<100; i+=0.18f){   
+
+ rotate(sin(millis()/100)+cos(second()*i+10)-tan(i/100)+atan(i/10));
+  drawFlower(i,i, 8);
+ drawFlower(-i/10,i/10,10);
+
+}
+popMatrix();
+println(second());
+println(millis());
+//println(mouseX);
+saveFrame("wc-#####.jpg");  
+}
+
+
+
+
+ 
+
+
+
+public void drawFlower(float x, float y,float radius){
+  noStroke();
+  colorMode(HSB);
+  
+ // fill(0,map(y,height,20,0.2,50),map(x,width,20,0.2,200),40);    //blue
+   int col = color(round(map(x,0.1f,width/10,10,255)), angle, 255,75); //rainbow color code
+   fill(col);
+ diameter = 2;
+ float radius1 = 1 +  (cos(angle/mouseX) * diameter/4) + sin(angle/mouseX);
+float radius2 = diameter- diameter*sin(angle/mouseX) + sin(angle/mouseX)*sqrt(abs(cos(angle/mouseY))/(sin(angle/mouseX)+1.4f));
+ float radius3 = 1 + (cos(angle * millis()) * diameter/4)+sin(millis());
+ x = radius * cos(angle/millis()*15);
+ y = radius * sin(angle/millis()*15);
+ float x1 =  10 * sin(angle/millis()*10);
+float  y1 = 10 * cos(angle/millis()*10);
+   float thisx = r * cos(theta);
+  float thisy =  r * sin(theta);
+
+  // Increment the angle
+  theta += 0.01f;
+
+  r += 0.1f;
+
+translate(x,y);
+pushMatrix();
+rotate(thisx);
+ellipse(thisx, thisy, 3, 3); 
+star(thisx, thisy,1,2,5);
+point(x+radius1,x+radius1,thisx);
+star(x,y,3,6,5);
+popMatrix();
+translate(x1,y1);
+stroke(col);
+pushMatrix();
+rotate(thisy);
+star(0,0,3,16,5);
+float a =lerp(x1,y1,radius1);
+float b = lerp(x1,y1, radius2);
+rotate(a);
+point(a,b);
+rotate(thisx);
+point(thisx,thisy);
+rotate(thisy);
+point(thisy+a, thisx+b);
+popMatrix();
+pushMatrix();
+rotate(radius);
+star(thisx, thisy, 3, 5, 5);
+//point(x/mouseX,mouseX/10+y,radius);
+strokeWeight(radius/2);
+popMatrix();
+angle += 0.15f;
+radius +=1;
+radius1 +=1;
+radius2 +=1;
+ }
+public void star(float x, float y, float radius1, float radius2, int npoints) {
+  float angle = TWO_PI / npoints;
+  float halfAngle = angle/2.0f;
+  beginShape();
+  for (float a = 0; a < TWO_PI; a += angle) {
+    float sx = x + cos(a) * radius2;
+    float sy = y + sin(a) * radius2;
+    point(sx, sy);
+    sx = x + cos(a+halfAngle) * radius1;
+    sy = y + sin(a+halfAngle) * radius1;
+    point(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
+  
+  
+
+ 
+  public void settings() {  size(1200,1200,P2D);  smooth(); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "--present", "--window-color=#666666", "--hide-stop", "ribbons_" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
